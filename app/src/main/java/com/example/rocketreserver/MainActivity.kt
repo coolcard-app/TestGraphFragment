a1e154a13658e41752749f65048c0804c232c66d
daniel-tsai <brightdanielt@gmail.com>
2021-02-25 21:00:10 +0800

    Ask two query in MainActivity.

--- app/src/main/java/com/example/rocketreserver/MainActivity.kt
+++ app/src/main/java/com/example/rocketreserver/MainActivity.kt
@@ -1,43 +1,68 @@
 package com.example.rocketreserver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.apollographql.apollo.coroutines.toFlow
-import com.example.rocketreserver.R
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.retry
-import kotlinx.coroutines.flow.retryWhen
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.await
+import com.example.rocketreserver.fragment.LaunchConnectionGraphFrag
+import com.example.rocketreserver.fragment.LaunchGraphFrag
+import com.example.rocketreserver.fragment.MissionGraphFrag
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        val client = ApolloClient.builder()
+            .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com")
+            .build()
+
         lifecycleScope.launch {
-            apolloClient(this@MainActivity).subscribe(TripsBookedSubscription()).toFlow()
-                .retryWhen { _, attempt ->
-                    delay(attempt * 1000)
-                    true
-                }
-                .collect {
-                    val trips = it.data?.tripsBooked
-                    val text = when {
-                        trips == null -> getString(R.string.subscriptionError)
-                        trips == -1 -> getString(R.string.tripCancelled)
-                        else -> getString(R.string.tripBooked, trips)
-                    }
-                    Snackbar.make(
-                        findViewById(R.id.main_frame_layout),
-                        text,
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                }
+            val responseA = client.query(LaunchListAQuery()).await()
+            val responseB = client.query(LaunchListBQuery()).await()
+
+            /**
+             * 在 graphql 使用 fragment 後，雖然兩個 query 查詢的內容一樣，
+             * 但 [LaunchListAQuery.Data] 與 [LaunchListBQuery.Data] 型別判斷仍然不一樣，
+             * 必須再尋找其子類別
+             * [LaunchListAQuery.Data.launches.fragments.launchConnectionGraphFrag]
+             * [LaunchListBQuery.Data.launches.fragments.launchConnectionGraphFrag]
+             * 方可得到共用的 data [LaunchConnectionGraphFrag]
+             * */
+            val launchListDataA = responseA.data
+            val launchListA = toLaunchList(
+                launchListDataA?.launches?.fragments?.launchConnectionGraphFrag
+            )
+            printLaunchList(launchListA)
+            val launchListDataB = responseB.data
+            val launchListB = toLaunchList(
+                launchListDataB?.launches?.fragments?.launchConnectionGraphFrag
+            )
+            printLaunchList(launchListB)
         }
+    }
+
+    private fun toLaunchList(frag: LaunchConnectionGraphFrag?): List<LaunchGraphFrag?>? {
+        return frag?.launches?.map { it?.fragments?.launchGraphFrag }
+    }
+
+    private fun printLaunchList(list: List<LaunchGraphFrag?>?) {
+        list?.forEach { printLaunch(it) }
+    }
+
+    private fun printLaunch(launch: LaunchGraphFrag?) {
+        val id = launch?.id
+        printMission(launch?.mission?.fragments?.missionGraphFrag)
+        val site = launch?.site
+        Log.d("Launch", "id:${id}, site:${site}")
+    }
+
+    private fun printMission(mission: MissionGraphFrag?) {
+        val name = mission?.name
+        Log.d("Mission", "name:${name}")
     }
 }
